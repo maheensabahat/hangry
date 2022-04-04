7332c15e9911aa5b20a9ece7dda4622465e0b398
Aveem1 <aveemmemon1@gmail.com>
2022-04-04 14:56:35 +0500

    Admin Side.
    Admin Side.

---
+++ lib/Approved_Signups.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Approved_Signups extends StatefulWidget {
+  const Approved_Signups({Key? key}) : super(key: key);
+
+  @override
+  _Approved_SignupsState createState() => _Approved_SignupsState();
+}
+
+class _Approved_SignupsState extends State<Approved_Signups> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Rejected_Signups.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Rejected_Signups extends StatefulWidget {
+  const Rejected_Signups({Key? key}) : super(key: key);
+
+  @override
+  _Rejected_SignupsState createState() => _Rejected_SignupsState();
+}
+
+class _Rejected_SignupsState extends State<Rejected_Signups> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
