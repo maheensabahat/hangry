1372782e5f8515929133dcad6f9cdce6c813428d
hammad-m <m.hammad_19784@khi.iba.edu.pk>
2022-04-09 23:17:37 +0500

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:project/RestaurantDetails.dart';
 import 'package:project/temporary_main_page.dart';
 import 'package:provider/provider.dart';
 
-// <<<<<<< HEAD
 import 'package:project/Order_history.dart';
 
 // import 'package:project/Signup_View.dart';
@@ -12,14 +11,12 @@ import 'package:project/Signup_history.dart';
 import 'package:project/Table_reservation.dart';
 
 // import 'package:project/user_signup.dart';
-// =======
 //import 'package:project/user_signup.dart';
 import 'Entities/cart.dart';
 import 'Favorites.dart';
 import 'MyOrders.dart';
 import 'package:project/Views/User/user_signup.dart';
 
-// >>>>>>> 90eb3c31f2d15043ea98f858ef6d4e48c6f46be5
 import 'RestaurantMenu.dart';
 import 'RestaurantEditDish.dart';
 import 'RestaurantOrder.dart';
@@ -28,9 +25,11 @@ import 'Views/User/UserMenu.dart';
 import 'Views/User/UserMenu.dart';
 
 void main() {
-  ChangeNotifierProvider(
-    create: (_) => cart(),
-    child: const MyApp(),
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => cart(),
+      child: const MyApp(),
+    ),
   );
 }
 
@@ -47,7 +46,7 @@ class MyApp extends StatelessWidget {
       // darkTheme: ThemeData.dark(),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
       //home: RestaurantMenu(),
-      home: TempMain(),
+      home: const TempMain(),
       //home: const UserMenuBeforeScanning(),
       //home: const ScanQR(),
       // home: const RestaurantMenu(),
