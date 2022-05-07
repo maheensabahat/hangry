646bbe264b41ba3d273e29f92ad1f7ab0439c9a2
Aveem1 <aveemmemon1@gmail.com>
2022-05-07 12:47:44 +0500

    Restaurant items

---
+++ lib/Entities/Products.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Products {
+  String? productName;
+  String? productImage;
+  int? price;
+
+  Products(this.productName, this.productImage, this.price);
+}
